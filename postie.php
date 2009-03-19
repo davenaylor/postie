dfb5ae29dac485c8bf4335ee71439601cdcdbc92
robfelty <robfelty@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-19 19:24:01 +0000

    updated todos
    git-svn-id: https://plugins.svn.wordpress.org/postie/trunk@103543 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- postie.php
+++ postie.php
@@ -9,11 +9,11 @@ Author URI: http://blog.robfelty.com/
 */
 
 /*
+$Id$
 * -= Requests Pending =-
 * German Umlats don't work
 * Problems under PHP5 
 * Problem with some mail server
-* Config Form freaks out in some cases
 * Multiple emails should tie to a single account
 * Each user should be able to have a default category
 * WP Switcher not compatible
@@ -25,7 +25,6 @@ Author URI: http://blog.robfelty.com/
     - os of server
     - os of client
     - number of users posting
-* make sure it handles the case where the url is http://www.site.com/wordpress/ instead of just http://www.site.com/
 * Test for calling from the command line
 * Support userid/domain  as a valid username
 * WP-switcher not compatiable http://www.alexking.org/index.php?content=software/wordpress/content.php#wp_120
@@ -35,7 +34,7 @@ Author URI: http://blog.robfelty.com/
 * Add support for http://unknowngenius.com/wp-plugins/faq.html#one-click
 *    www.cdavies.org/code/3gp-thumb.php.txt
 *    www.cdavies.org/permalink/watchingbrowserembeddedgpvideosinlinux.php
-* Support draft/private posts
+* Support private posts
 * Make it possible to post without a script at all
 */
 
