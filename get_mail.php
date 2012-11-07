f85b95e103b332f976a94acab3216c8556968401
WayneAllen <WayneAllen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-07 22:50:48 +0000

    Fixed bug in XSS attack vulnerability code. Thanks to R Reid http://blog.strictly-software.com/2012/03/fixing-postie-plugin-for-wordpress-to.html Fixed bug where emails with multiple categories has the incorrect title
    git-svn-id: https://plugins.svn.wordpress.org/postie/trunk@622321 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- get_mail.php
+++ get_mail.php
@@ -1,15 +1,14 @@
 <?php
 
 //Load up some usefull libraries
-include_once (dirname(dirname(dirname(dirname(__FILE__)))) .  DIRECTORY_SEPARATOR."wp-config.php");
-require_once (dirname(__FILE__). DIRECTORY_SEPARATOR . 'mimedecode.php');
-require_once (dirname(__FILE__). DIRECTORY_SEPARATOR . 'postie-functions.php');
-	
+include_once (dirname(dirname(dirname(dirname(__FILE__)))) . DIRECTORY_SEPARATOR . "wp-config.php");
+require_once (dirname(__FILE__) . DIRECTORY_SEPARATOR . 'mimedecode.php');
+require_once (dirname(__FILE__) . DIRECTORY_SEPARATOR . 'postie-functions.php');
+
 
 /* END OF USER VARIABLES */
 //some variables
 //error_reporting(2037);
-
 //Retreive emails 
 print("<pre>\n");
 print("This is the postie plugin\n");
@@ -17,53 +16,51 @@ print("time:" . time() . "\n");
 include('Revision');
 $config = get_option('postie-settings');
 extract($config);
-$emails = FetchMail($mail_server, $mail_server_port, $mail_userid, 
-    $mail_password, $input_protocol, $time_offset, $test_email,
-    $delete_mail_after_processing);
+$emails = FetchMail($mail_server, $mail_server_port, $mail_userid, $mail_password, $input_protocol, $time_offset, $test_email, $delete_mail_after_processing);
 //loop through messages
 foreach ($emails as $email) {
-  if (function_exists('memory_get_usage'))
-    echo "memory at start of e-mail processing:" . memory_get_usage() . "\n";
+    if (function_exists('memory_get_usage'))
+        echo "memory at start of e-mail processing:" . memory_get_usage() . "\n";
     //sanity check to see if there is any info in the message
-    if ($email == NULL ) { 
-      $message= __('Dang, message is empty!', 'postie'); 
-      continue; 
-    } else if ($email=='already read') {
-      $message = "\n" . __("There does not seem to be any new mail.", 'postie') .
-      "\n";
-      continue;
+    if ($email == NULL) {
+        $message = __('Dang, message is empty!', 'postie');
+        continue;
+    } else if ($email == 'already read') {
+        $message = "\n" . __("There does not seem to be any new mail.", 'postie') .
+                "\n";
+        continue;
     }
- // check for XSS attacks - we disallow any javascript, meta, onload, or base64
-    if (preg_match("/.*(script|onload|meta).*/is", $email)) {
-      echo "possible XSS attack - ignoring email\n";
-      continue;
+    // check for XSS attacks - we disallow any javascript, meta, onload, or base64
+    if (preg_match("@((%3C|<)/?script|<meta|document\.|\.cookie|\.createElement|onload\s*=|(eval|base64)\()@is", $email)) {
+        echo "possible XSS attack - ignoring email\n";
+        continue;
     }
 
-    $message='';
+    $message = '';
     $mimeDecodedEmail = DecodeMIMEMail($email, true);
     $from = RemoveExtraCharactersInEmailAddress(trim($mimeDecodedEmail->headers["from"]));
     /*
-    if ($from != "") {
-        continue;
-    }
-    */
+      if ($from != "") {
+      continue;
+      }
+     */
 
     //Check poster to see if a valid person
     $poster = ValidatePoster($mimeDecodedEmail, $config);
     if (!empty($poster)) {
-        if ($test_email) 
-          DebugEmailOutput($email,$mimeDecodedEmail); 
-        PostEmail($poster,$mimeDecodedEmail, $config);
+        if ($test_email)
+            DebugEmailOutput($email, $mimeDecodedEmail);
+        PostEmail($poster, $mimeDecodedEmail, $config);
     }
     else {
         print("<p>Ignoring email - not authorized.\n");
     }
-  if (function_exists('memory_get_usage'))
-    echo "memory at end of e-mail processing:" . memory_get_usage() . "\n";
+    if (function_exists('memory_get_usage'))
+        echo "memory at end of e-mail processing:" . memory_get_usage() . "\n";
 } // end looping over messages
 print $message;
 print("</pre>\n");
-    
+
 /* END PROGRAM */
 
 // end of script
